39d873864fdbc1bc62a8fac89d5e578578b9eba4
Stanley <stagebyte45@gmail.com>
2021-05-07 15:33:31 +0100

    added a folder for testing and modified the meadowlark.js file

--- meadowlark.js
+++ meadowlark.js
@@ -15,6 +15,13 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000)
 
+// url for page testing & debugging
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+    next();
+    });
+
 // home page
 app.get('/', (req, res) => {
     //res.type('text/plain');
@@ -29,8 +36,28 @@ app.get('/about', (req, res) => {
     //res.type('text/plain');
     //res.send('About Meadowlark Travel');
     //res.render('about', {fortune: randomFortune})
-    res.render('about', {fortune: fortune.getFortune() })
+    res.render('about', {
+                            fortune: fortune.getFortune(),
+                            pageTestScript: '/qa/tests-about.js' 
+                        })
 })
+
+// routes for tour section
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+
+// getting information about the headers
+app.get('/headers', function(req,res){
+    res.set('Content-Type','text/plain');
+    var s = '';
+    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
 
 // custom 404 page
 // 404 catch-all handler (middleware)
@@ -49,7 +76,10 @@ app.use((err, req, res, next)=>{
     res.status(500);
     //res.send('500 - Server Error');
     res.render('500')
+    //res.status(500).render('500')
 })
+
+app.disable('x-powered-by');
 
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
